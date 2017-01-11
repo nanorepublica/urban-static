45ee115ba7b226bda306a053937493030e705fb7
Andrew Miller <andrew.miller@founders4schools.org.uk>
2017-01-11 09:11:28 +0000

    use the assets folder

--- _layouts/script.js
+++
@@ -1,5 +0,0 @@
-{% include js/libs/jquery.js %}
-{% include js/libs/bootstrap.min.js %}
-{% include js/libs/jquery.easing.min.js %}
-{% include https://maps.googleapis.com/maps/api/js?key=AIzaSyCRngKslUGJTlibkQ3FkfTxj3Xss1UlZDA %}
-{% include js/urban-static.js %}
