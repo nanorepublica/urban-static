b40f7aafd09f038ebaaa40dbd066181960994212
Andrew Miller <andrew.miller@founders4schools.org.uk>
2017-01-11 17:48:42 +0000

    more setting up

--- js/urban-static.js
+++ js/urban-static.js
@@ -299,34 +299,36 @@ function inithome() {
         var autoOpen = true;
         var zoom = 15;
     }
-    var mapOptions = {
-        // How zoomed in you want the map to start at (always required)
-        // zoom: 15,
-        zoom: zoom,
-        maxZoom: zoom,
-        minZoom: zoom,
+    try {
+        var mapOptions = {
+            // How zoomed in you want the map to start at (always required)
+            // zoom: 15,
+            zoom: zoom,
+            maxZoom: zoom,
+            minZoom: zoom,
 
-        // The latitude and longitude to center the map (always required)
-        center: new google.maps.LatLng(52.2006602, 0.14),
+            // The latitude and longitude to center the map (always required)
+            center: new google.maps.LatLng(52.2006602, 0.14),
 
-        // Disables the default Google Maps UI components
-        disableDefaultUI: true,
-        scrollwheel: false,
-        draggable: false,
+            // Disables the default Google Maps UI components
+            disableDefaultUI: true,
+            scrollwheel: false,
+            draggable: false,
 
-        // How you would like to style the map.
-        // This is where you would paste any style found on Snazzy Maps.
-        styles: mapStyles
+            // How you would like to style the map.
+            // This is where you would paste any style found on Snazzy Maps.
+            styles: mapStyles
 
-    };
+        };
 
-    // Get the HTML DOM element that will contain your map
-    // We are using a div with id="map" seen below in the <body>
-    var mapElement = document.getElementById('map');
+        // Get the HTML DOM element that will contain your map
+        // We are using a div with id="map" seen below in the <body>
+        var mapElement = document.getElementById('map');
 
-    // Create the Google Map using out element and options defined above
-    if (mapElement !== null) {
-      var map = new google.maps.Map(mapElement, mapOptions);
+        // Create the Google Map using out element and options defined above
+        var map = new google.maps.Map(mapElement, mapOptions);
+    } throw (ReferenceError) {
+        console.log('google not defined, no maps today!');
     }
 
 }
--- urban-static.gemspec
+++ urban-static.gemspec
@@ -11,7 +11,7 @@ Gem::Specification.new do |spec|
   spec.homepage      = "https://github.com/urban-static"
   spec.license       = "MIT"
 
-  spec.files         = `git ls-files -z`.split("\x0").select { |f| f.match(%r{^(_layouts|_includes|_sass|LICENSE|README)/i}) }
+  spec.files         = `git ls-files -z`.split("\x0").select { |f| f.match(%r{^(_layouts|_includes|_sass|assets|LICENSE|README)/i}) }
 
   spec.add_development_dependency "jekyll", "~> 3.2"
   spec.add_development_dependency "bundler", "~> 1.12"
