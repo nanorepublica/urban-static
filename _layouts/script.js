9e41d01e8e5731945616f48cbe9df3c3ad6b7360
Andrew Miller <andrew.miller@founders4schools.org.uk>
2017-01-11 08:45:39 +0000

    add javascript

---
+++ _layouts/script.js
@@ -0,0 +1,5 @@
+{% include js/libs/jquery.js %}
+{% include js/libs/bootstrap.min.js %}
+{% include js/libs/jquery.easing.min.js %}
+{% include https://maps.googleapis.com/maps/api/js?key=AIzaSyCRngKslUGJTlibkQ3FkfTxj3Xss1UlZDA %}
+{% include js/urban-static.js %}
